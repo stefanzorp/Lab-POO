83534d027406aefcbe442979cfd6d88ad27d4aa0
stefanzorp <budaustefan405@gmail.com>
2025-05-30 02:38:35 +0300

    Add files via upload

--- proiect/main.cpp
+++ proiect/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+
 using namespace std;
 
-int main(){
-    
+#include "files/Acvariu.h"
+#include "files/Barca.h"
+#include "files/Peste.h"
+#include "files/User.h"
+#include "files/Pescar.h" 
+#include "files/Mancare.h" 
+
+int main() {
+    Barca barca;
+    barca.Ruleaza();  
     return 0;
 }
